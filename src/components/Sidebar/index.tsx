15db0c1bd7c717b5449ad207f5e7370af136e206
Bảo <monkeyd.lucy2110@gmail.com>
2022-07-16 16:03:03 +0700

    fix active menu on desktop view

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -62,6 +62,7 @@ export const MobileSidebar = () => {
 
 export const Sidebar = () => {
     const { t } = useTranslation()
+    const {home, explore, create, auction} = useChangeRoute()
 
     return (
         <div className="fixed top-0 left-0 bottom-0 z-10 bg-light-gray-0 dark:bg-dark-gray-90 flex flex-col gap-2">
@@ -72,33 +73,37 @@ export const Sidebar = () => {
                 <li className="px-2 py-4 hover:bg-light-gray-20 dark:hover:bg-dark-gray-50">
                     <Link to="/" className="flex flex-col justify-center gap-2">
                         <div className="flex justify-center">
-                            <AiFillHome className="w-6 h-6 text-gray-900 dark:text-white cursor-pointer"/>
+                            { !home && <AiOutlineHome className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
+                            { home && <AiFillHome className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
                         </div>
-                        <p className="text-center text-xs">{ t('home') }</p>
+                        <p className={`text-center text-xs ${home ? 'font-bold' : ''}`}>{ t('home') }</p>
                     </Link>
                 </li>
                 <li className="px-2 py-4 hover:bg-light-gray-20 dark:hover:bg-dark-gray-50">
                     <Link to="/explore" className="flex flex-col justify-center gap-2">
                         <div className="flex justify-center">
-                            <AiOutlineCompass className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/>
+                            { !explore && <AiOutlineCompass className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
+                            { explore && <AiFillCompass className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
                         </div>
-                        <p className="text-center text-xs">{ t('explore') }</p>
+                        <p className={`text-center text-xs ${explore ? 'font-bold' : ''}`}>{ t('explore') }</p>
                     </Link>
                 </li>
                 <li className="px-2 py-4 hover:bg-light-gray-20 dark:hover:bg-dark-gray-50">
                     <Link to="/create" className="flex flex-col justify-center gap-2">
                         <div className="flex justify-center">
-                            <AiOutlinePayCircle className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/>
+                            { !create && <AiOutlinePayCircle className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
+                            { create && <AiFillPayCircle className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
                         </div>
-                        <p className="text-center text-xs">{ t('sell') }</p>
+                        <p className={`text-center text-xs ${create ? 'font-bold' : ''}`}>{ t('sell') }</p>
                     </Link>
                 </li>
                 <li className="px-2 py-4 hover:bg-light-gray-20 dark:hover:bg-dark-gray-50">
                     <Link to="/auction" className="flex flex-col justify-center gap-2">
                         <div className="flex justify-center">
-                            <AiOutlineBank className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/>
+                            { !auction && <AiOutlineBank className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
+                            { auction && <AiFillBank className="w-6 h-6 text-dark-gray-100 dark:text-white cursor-pointer"/> }
                         </div>
-                        <p className="text-center text-xs">{ t('auction') }</p>
+                        <p className={`text-center text-xs ${auction ? 'font-bold' : ''}`}>{ t('auction') }</p>
                     </Link>
                 </li>
             </ul>
